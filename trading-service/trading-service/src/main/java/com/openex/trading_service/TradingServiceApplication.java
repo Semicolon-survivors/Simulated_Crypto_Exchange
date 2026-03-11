9ec46b4bd8009e3de9dd947cad801f8aeb167a9a
Rodney <rodneynathi2@gmail.com>
2026-03-11 10:23:01 +0200

    Trading Service module Version 2

--- trading-service/trading-service/src/main/java/com/openex/trading_service/TradingServiceApplication.java
+++ trading-service/trading-service/src/main/java/com/openex/trading_service/TradingServiceApplication.java
@@ -2,15 +2,9 @@ package com.openex.trading_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(
-        scanBasePackages = "com.openex",
-        exclude = {
-                org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class,
-                org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration.class,
-                org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration.class
-        }
-)
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class TradingServiceApplication {
 
 	public static void main(String[] args) {
